2b185c3edae9bddecb07209e5d9b17b81d08f9f6
Chris Hintz <chrishintz42@gmail.com>
2016-01-22 10:23:52 -0800

    Add os and PORT env var

--- app/views.py
+++ app/views.py
@@ -1,3 +1,4 @@
+import os
 from flask import render_template
 from app import app
 
@@ -5,3 +6,7 @@ from app import app
 @app.route('/index')
 def index():
     return render_template('layout.html')
+
+if __name__ == "__main__":
+    port = os.environ.get("PORT") or 5000
+    app.run("0.0.0.0", port)
